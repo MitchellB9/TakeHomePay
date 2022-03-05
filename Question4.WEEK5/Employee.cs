318b1da9b4b1080ee5c22e83fa4e9993432e933d
MitchellB9 <mitchellr418@yahoo.com>
2022-03-05 14:22:28 -0500

    Add project files.

---
+++ Question4.WEEK5/Employee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Question4.WEEK5
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmpID { get; set; }
+        public int Salary { get; set; }
+        public int Hourly { get; set; }
+        public int HoursClocked { get; set; }
+
+
+        public double TakeHomePay()
+        {
+            double finalPay;
+            double timeHalfPay = 0;
+            int timeAndHalf = 0;
+
+            if(Hourly > Salary)
+            {
+                if(HoursClocked > 40)
+                {
+                    timeAndHalf = HoursClocked - 40;
+                    timeHalfPay = timeAndHalf * (Hourly * 1.5);
+                }
+                finalPay = (Hourly * (HoursClocked - timeAndHalf)) + timeHalfPay;
+                return finalPay - Retirement(finalPay) - SocialS(finalPay) - FedTax(finalPay);
+            }
+            else
+            {
+                finalPay = Salary - Retirement(Salary) - SocialS(Salary) - FedTax(Salary);
+                return finalPay;
+            }
+        }
+
+        public double Retirement(double pay)
+        {
+            double ret;
+            ret = pay * 0.10;
+            return ret;
+        }
+        public double SocialS(double pay)
+        {
+            double soc;
+            soc = pay * 0.06;
+            return soc;
+        }
+
+        public double FedTax(double pay)
+        {
+            double fed;
+            fed = pay * 0.18;
+            return fed;
+        }
+
+        public override string ToString()
+        {
+            return "\nEmployee name: " + FirstName + " " + LastName + "\nEmployee ID: " + EmpID + "\nThe employee takes home $" + TakeHomePay();
+        }
+
+
+
+    }
+}
---
+++ Question4.WEEK5/Program.cs
@@ -0,0 +1,53 @@
+﻿/*	Program that calculates the take-home pay for an employee. 
+ *	The two types of employees are salaried and hourly. 
+ *	Allow the user to input the employee’s first and last name, id, and type. 
+ *	If an employee is salaried, allow the user to input the salary amount. 
+ *	If an employee is hourly, allow the user to input the hourly rate and the number of hours clocked for the week. 
+ *	For hourly employees, overtime is paid for hours over 40 at a rate of 1.5 of the base rate. 
+ *	For all employees’ take-home pay, federal tax of 18% is deducted. 
+ *	A retirement contribution of 10% and a Social Security tax rate of 6% should also be deducted.
+ */
+
+
+using System;
+
+namespace Question4.WEEK5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yn;
+            Employee employee = new Employee();
+
+            Console.WriteLine("Enter the Employee's first name: ");
+            employee.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter the Employee's last name: ");
+            employee.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter the Employee's ID number: ");
+            employee.EmpID = Console.ReadLine();
+
+            Console.WriteLine("Is the Employee paid hourly? (y/n): ");
+            yn = Console.ReadLine();
+            if (yn == "y" || yn == "Y")
+            {
+                employee.Salary = 0;
+                Console.WriteLine("Enter how many dollars per hour the emploee makes: ");
+                employee.Hourly = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter how many hours the employee clocked in: ");
+                employee.HoursClocked = Convert.ToInt32(Console.ReadLine());
+            }
+            else
+            {
+                employee.Hourly = 0;
+                Console.WriteLine("Enter the employee's salary: ");
+                employee.Salary = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine(employee.ToString());
+        }
+    }
+}
